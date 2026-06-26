9a54b68fa08ffd60afc1af5a7d4e02897e13efc9
saba <schabakauri4@gmail.com>
2026-06-26 16:29:26 +0400

    Add files via upload

---
+++ PROJECT/script.js
@@ -0,0 +1,11 @@
+
+const burgerBtn = document.querySelector('.burger-menu');
+const navMenu = document.querySelector('.links');
+
+
+burgerBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('open');
+});
+
+
+
